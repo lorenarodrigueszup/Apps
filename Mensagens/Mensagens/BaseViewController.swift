973fe2f8a5ac7a3a15fe2d56890a7c0de8421345
lorenarodrigueszup <lorena.rodrigues@zup.com.br>
2019-02-01 17:05:00 -0200

    App mensagens

---
+++ Mensagens/Mensagens/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  Mensagens
+//
+//  Created by Lorena Rodrigues on 01/02/19.
+//  Copyright © 2019 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    @IBOutlet weak var lbMessage: UILabel!
+    var message: Message!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func changeColor(_ sender: UIButton) {
+        if let reference = self as? ColorPickerDelegate {
+            let colorPicker = storyboard?.instantiateViewController(withIdentifier: "ColorPickerViewController") as! ColorPickerViewController
+            colorPicker.modalPresentationStyle = .overCurrentContext
+            colorPicker.delegate = reference
+            present(colorPicker, animated: true, completion: nil)
+        }
+
+    }
+
+}
