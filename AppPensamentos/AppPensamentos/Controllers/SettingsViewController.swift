b7496fa058282d45ba12b7ab9ac43d0b7d064f59
lorenarodrigueszup <lorena.rodrigues@zup.com.br>
2019-02-06 11:34:02 -0200

    App de pensamentos

--- AppPensamentos/AppPensamentos/Controllers/SettingsViewController.swift
+++ AppPensamentos/AppPensamentos/Controllers/SettingsViewController.swift
@@ -10,21 +10,49 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var swAutorefresh: UISwitch!
+    @IBOutlet weak var lbTimeInterval: UILabel!
+    @IBOutlet weak var slTimeInterval: UISlider!
+    @IBOutlet weak var scColorScheme: UISegmentedControl!
+
+    let config = Configuration.shared
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "Refresh"), object: nil, queue: nil) { (notification) in
+            self.formatView()
+        }
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        formatView()
+    }
+
+    func formatView() {
+        swAutorefresh.setOn(config.autorefresh, animated: false)
+        slTimeInterval.setValue(Float(config.timeInterval), animated: false)
+        scColorScheme.selectedSegmentIndex = config.colorScheme
+        changeTimeIntervalLabel(with: config.timeInterval)
+    }
+
+    func changeTimeIntervalLabel(with value: Double) {
+        lbTimeInterval.text = "Mudar após \(Int(value)) segundos"
+    }
+
+    @IBAction func changeAutorefresh(_ sender: UISwitch) {
+        config.autorefresh = sender.isOn
+    }
+
+    @IBAction func changeTimeInterval(_ sender: UISlider) {
+        let value = Double(round(sender.value))
+        changeTimeIntervalLabel(with: value)
+        config.timeInterval = value
+    }
+
+    @IBAction func changeColorScheme(_ sender: UISegmentedControl) {
+        config.colorScheme = sender.selectedSegmentIndex
+    }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- AppPensamentos/AppPensamentos/Models/Quote.swift
+++ AppPensamentos/AppPensamentos/Models/Quote.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+struct Quote: Codable {
+    let quote: String
+    let author: String
+    let image: String
+
+    var quoteFormatted: String {
+        return "❝" + quote + "❞"
+    }
+
+    var authorFormatted: String {
+        return "- " + author + " -"
+    }
+}
